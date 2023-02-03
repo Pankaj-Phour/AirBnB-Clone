93e89b05bd46d6d29fcb52554a70cf051623c485
Pankaj Phour <pankaj.phour70@gmail.com>
2023-02-03 19:04:08 +0530

    First push

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { MainComponent } from './main/main.component';
+import { TopComponent } from './top/top.component';
+
+
+@NgModule({
+  declarations: [
+    AuthenticationComponent,
+    MainComponent,
+    TopComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
